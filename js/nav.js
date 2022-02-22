4f008764bffecbc9f041cd31202a06998bbf180a
arttop1000 <arthurbarbosams@yahoo.com>
2022-02-22 01:46:46 -0300

    InicialCommit

---
+++ js/nav.js
@@ -0,0 +1,24 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+    <div class="nav">
+    <img src="img/DigMA.png" class="brand-logo" alt="">
+    <div class="nav-itens">
+        <div class="search">
+            <input type="text" class="search-box" placeholder="Pesquise Serviços Aqui">
+            <button class="search-btn">search</button>
+        </div>
+        <a href="#"><img src="img/user.png" alt=""></a>
+        <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+</div>
+<ul class="links-container">
+    <li class="link-item"><a href="#" class="link">Início</a></li>
+    <li class="link-item"><a href="#" class="link">Como Funciona</a></li>
+    <li class="link-item"><a href="#" class="link">Serviços</a></li>
+</ul>
+    `;
+}
+
+createNav();
