53398b5941dc6e6d3be257af1e524e61504568b4
arttop1000 <arthurbarbosams@yahoo.com>
2022-03-01 22:16:22 -0300

    footerFinish

---
+++ js/footer.js
@@ -0,0 +1,105 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    
+    <div class="footer-content">
+    <img src="./img/DigMA (1).png" class="logo" alt="">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">Elétrica</li>
+            <li>
+                <a href="" class="footer-link">tomadas<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">fios</a>
+            <li>
+            <li>
+                <a href="" class="footer-link">aparelho<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">Chuveiro<a>
+            </li>
+            <li><a href="" class="footer-link">Projeto ART</a></li>
+
+        </ul>
+        <ul class="category">
+            <li class="category-title">Hidraulica</li>
+            <li>
+                <a href="" class="footer-link">tomadas<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">fios</a>
+            <li>
+            <li>
+                <a href="" class="footer-link">aparelho<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">Chuveiro<a>
+            </li>
+            <li><a href="" class="footer-link">Projeto ART</a></li>
+
+        </ul>
+        <ul class="category">
+            <li class="category-title">Acabamento</li>
+            <li>
+                <a href="" class="footer-link">tomadas<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">fios</a>
+            <li>
+            <li>
+                <a href="" class="footer-link">aparelho<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">Chuveiro<a>
+            </li>
+            <li><a href="" class="footer-link">Projeto ART</a></li>
+
+        </ul>
+        <ul class="category">
+            <li class="category-title">Pintura</li>
+            <li>
+                <a href="" class="footer-link">tomadas<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">fios</a>
+            <li>
+            <li>
+                <a href="" class="footer-link">aparelho<a>
+            </li>
+            <li>
+                <a href="" class="footer-link">Chuveiro<a>
+            </li>
+            <li><a href="" class="footer-link">Projeto ART</a></li>
+
+        </ul>
+    </div>
+</div>
+<p class="footer-title">Sobre Nós</p>
+<p class="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur omnis tempore ullam asperiores
+    architecto autem quisquam nisi deleniti distinctio maxime hic ducimus minus commodi repellendus porro itaque
+    iure ipsum, repudiandae beatae neque perferendis adipisci? Eveniet aperiam distinctio veniam pariatur
+    obcaecati. Rerum nulla, explicabo quae nisi unde expedita eligendi obcaecati? Quia possimus nam sapiente?
+    Provident ducimus quo iusto autem blanditiis dicta velit cumque rerum officia minima cupiditate
+    exercitationem beatae voluptas sunt incidunt, amet, excepturi nulla! Officiis magni corrupti voluptas magnam
+    quas provident amet ab quis unde ea consequuntur eum facere fugit iure beatae, repudiandae enim? Animi
+    officiis qui dolores consequuntur quis.
+<P class="info">Email de Atendimento - atendimento@DigMA.com.br</P>
+<p class="info">Telefone 4044-4342</p>
+<div class="footer-social-container">
+    <div>
+        <a href="#" class="social-link">Termos de Uso</a>
+        <a href="#" class="social-link">Politica de Privacidade</a>
+    </div>
+    <div>
+        <a href="#" class="social-link">Facebook</a>
+        <a href="#" class="social-link">Instagram</a>
+        <a href="#" class="social-link">Twitter</a>
+    </div>
+    <p class="footer-credit">© 2022 DigMA - Todos os direitos reservados</p>
+</div>
+    `;
+}
+
+createFooter();
